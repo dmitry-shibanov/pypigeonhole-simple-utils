bfed1e83e84c504fe212d868fab1f5385af87879
Dmitry Shibanov <shibanov-1997@inbox.ru>
2020-10-05 17:13:55 +0300

    Update __init__.py

--- test/test_pypigeonhole_utils/__init__.py
+++ test/test_pypigeonhole_utils/__init__.py
@@ -1,10 +1,10 @@
-import os
-import sys
+# import os
+# import sys
 
-# This is to fix the src module path during unit testing from command line.
-# Really an ugly hack, not natural. The command line runner assume the current
-# folder as src.
-PROJECT_PATH = os.getcwd()
+# # This is to fix the src module path during unit testing from command line.
+# # Really an ugly hack, not natural. The command line runner assume the current
+# # folder as src.
+# PROJECT_PATH = os.getcwd()
 
-SOURCE_PATH = os.path.join(PROJECT_PATH, "src")
-sys.path.append(SOURCE_PATH)
+# SOURCE_PATH = os.path.join(PROJECT_PATH, "src")
+# sys.path.append(SOURCE_PATH)
